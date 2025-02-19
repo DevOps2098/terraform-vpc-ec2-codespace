e1f7fabda05f17f794a0fa6d3270aa539a777c4c
DevOps2098 <prasannalakshmi12345.tangala@gmail.com>
2025-02-19 12:29:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,20 @@ module "iam" {
   iam_user_name = "Prasanna-user"  # Existing IAM user
   account_id    = "491085415494"  # Replace with your actual AWS account ID
 }
+
+# VPC Module
+module "vpc" {
+  source             = "./modules/vpc"
+  vpc_cidr          = var.vpc_cidr
+  public_subnets    = var.public_subnets
+  private_subnets   = var.private_subnets
+  availability_zones = var.availability_zones
+}
+
+# EC2 Module
+module "ec2" {
+  source            = "./modules/ec2"
+  public_subnet_id  = module.vpc.public_subnet_ids[0]  # First public subnet
+  private_subnet_id = module.vpc.private_subnet_ids[0] # First private subnet
+  security_group_id = module.vpc.security_group_id
+}
