5ab93521b57ef7abebf62e8fc0630e84a1522142
DevOps2098 <prasannalakshmi12345.tangala@gmail.com>
2025-02-19 15:34:47 +0530

    Update variable.tf

--- modules/ec2/variable.tf
+++ modules/ec2/variable.tf
@@ -1,3 +1,18 @@
 variable "key_name" {
   default = "my-key-pair"
 }
+
+variable "vpc_security_group_ids" {
+  description = "VPC Security Group IDs"
+  type        = list(string)
+}
+
+variable "public_subnet_id" {
+  description = "Public Subnet ID"
+  type        = string
+}
+
+variable "private_subnet_id" {
+  description = "Private Subnet ID"
+  type        = string
+}
