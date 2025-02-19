39f6c4f36642271bd8417d5fb6db324eb5ed7cc6
DevOps2098 <prasannalakshmi12345.tangala@gmail.com>
2025-02-19 10:00:22 +0530

    Create variables.tf

---
+++ modules/iam/variables.tf
@@ -0,0 +1,11 @@
+variable "iam_user_name" {
+  description = "Name of the existing IAM user"
+  type        = string
+  default = "Prassa-user"
+}
+
+variable "account_id" {
+  description = "AWS Account ID"
+  type        = string
+  default = "491085415494"
+}
