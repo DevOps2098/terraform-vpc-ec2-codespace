5c8aa10e17c248af4c7989979f3a4001bc2793be
DevOps2098 <prasannalakshmi12345.tangala@gmail.com>
2025-02-19 15:16:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,19 +4,17 @@ module "iam" {
   account_id    = "491085415494"  # Replace with your actual AWS account ID
 }
 
-# VPC Module
 module "vpc" {
   source             = "./modules/vpc"
-  vpc_cidr          = var.vpc_cidr
-  public_subnets    = var.public_subnets
-  private_subnets   = var.private_subnets
   availability_zones = var.availability_zones
+  vpc_cidr_block     = var.vpc_cidr_block
 }
 
-# EC2 Module
 module "ec2" {
-  source            = "./modules/ec2"
-  public_subnet_id  = module.vpc.public_subnet_ids[0]  # First public subnet
-  private_subnet_id = module.vpc.private_subnet_ids[0] # First private subnet
-  vpc_security_group_ids = module.vpc.security_group_id
+  source                 = "./modules/ec2"
+  public_subnet_id       = module.vpc.public_subnet_ids[0]   # First public subnet ID
+  private_subnet_id      = module.vpc.private_subnet_ids[0]  # First private subnet ID
+  vpc_security_group_ids = [module.vpc.security_group_id]    # Security Group ID as a list
+  key_name               = var.key_name
 }
+
