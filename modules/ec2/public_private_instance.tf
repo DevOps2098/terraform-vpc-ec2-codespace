82c4c7c2bd849f1bc0d854c66561ce7418d096af
DevOps2098 <prasannalakshmi12345.tangala@gmail.com>
2025-02-19 15:10:12 +0530

    Update public_private_instance.tf

--- modules/ec2/public_private_instance.tf
+++ modules/ec2/public_private_instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "public" {
   ami             = "ami-0abcdef1234567890"
   instance_type   = "t2.micro"
   subnet_id       = var.public_subnet_id
-  security_groups = [var.security_group_id]
+  vpc_security_group_ids = [var.security_group_id]
   key_name        = var.key_name  # Add Key Pair
 
   tags = {
@@ -14,7 +14,7 @@ resource "aws_instance" "private" {
   ami             = "ami-0abcdef1234567890"
   instance_type   = "t2.micro"
   subnet_id       = var.private_subnet_id
-  security_groups = [var.security_group_id]
+  vpc_security_group_ids = [var.security_group_id]
   key_name        = var.key_name  # Add Key Pair
 
   tags = {
